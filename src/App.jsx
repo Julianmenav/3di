71605e7b5cbddafd5457fd1e07e2c212fcf0942c
Julianmenav <julianmenav2@gmail.com>
2022-10-06 21:18:04 +0200

    Add Canvas and textures

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { Physics } from "@react-three/cannon";
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Ground from "./components/Ground";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count {count}
-        </button>
-    </div>
-  )
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
+      </Canvas>
+    </>
+  );
 }
 
-export default App
+export default App;
