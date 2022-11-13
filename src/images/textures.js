63430267e4050b6b94435dd139eabe9eb845da71
Julianmenav <julianmenav2@gmail.com>
2022-11-13 21:29:34 +0100

    add Cubes components

--- src/images/textures.js
+++ src/images/textures.js
@@ -1,4 +1,4 @@
-import { TextureLoader } from "three";
+import { TextureLoader, NearestFilter, RepeatWrapping } from "three";
 import { dirtImg, grassImg, glassImg, logImg, woodImg } from "./images";
 
 const dirtTexture = new TextureLoader().load(dirtImg);
@@ -7,6 +7,15 @@ const grassTexture = new TextureLoader().load(grassImg);
 const glassTexture = new TextureLoader().load(glassImg);
 const logTexture = new TextureLoader().load(logImg);
 const groundTexture = grassTexture;
+
+dirtTexture.magFilter = NearestFilter;
+logTexture.magFilter = NearestFilter;
+grassTexture.magFilter = NearestFilter;
+glassTexture.magFilter = NearestFilter;
+woodTexture.magFilter = NearestFilter;
+groundTexture.magFilter = NearestFilter;
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
 
 export {
   dirtTexture,
